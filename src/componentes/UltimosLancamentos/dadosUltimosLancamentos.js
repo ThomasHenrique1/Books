7df2011d758756a8bfa006ab62667a385d787fbb
ThomasHenrique1 <thomasnhenrique@gmail.com>
2024-08-23 20:51:25 -0300

    Aprendizado em React
    Tudo foi feito com base no curso da Alura para o aprendizado em React

---
+++ src/componentes/UltimosLancamentos/dadosUltimosLancamentos.js
@@ -0,0 +1,7 @@
+import livro from '../../imagens/livro.png'
+
+export const livros = [
+    {nome: 'Liderança em Design', id: 1, src:livro},
+    {nome: 'Cangaceiro em JavaScript: Uma Aventura no Sertão', id: 3, src:livro},
+    {nome: 'Aventureiros do Java', id: 4, src:livro},
+]
